df251ed8be10a035ac42064f9ef2501eadbe28a0
lightningevil <narutoinwlp112@hotmail.com>
2021-09-09 11:55:45 +0700

    homework2

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameController : MonoBehaviour
+{
+    public Button okButton;
+    public InputField answerField;
+    
+    
+    void Start()
+    {
+        print("hell");
+    }
+
+    public void printName()
+    {
+        print("mark");
+    }
+    
+    void Update()
+    {
+        //print(answerField.text);
+    }
+}
